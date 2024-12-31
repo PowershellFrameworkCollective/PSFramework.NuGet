7da895d29a781a426b1e708734ce735a61220db6
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2024-12-31 12:23:59 +0100

    updates

--- PSFramework.NuGet/internal/functions/Get/Publish/Copy-Module.ps1
+++ PSFramework.NuGet/internal/functions/Get/Publish/Copy-Module.ps1
@@ -7,7 +7,7 @@
 		[string]
 		$Destination,
 
-		$Cmdlet,
+		$Cmdlet = $PSCmdlet,
 
 		[switch]
 		$Continue,
@@ -15,8 +15,65 @@
 		[string]
 		$ContinueLabel
 	)
+	begin {
+		$killIt = $ErrorActionPreference -eq 'Stop'
+		$stopCommon = @{
+			Cmdlet          = $Cmdlet
+			EnableException = $killIt
+		}
+		if ($Continue) { $stopCommon.Continue = $true }
+		if ($ContinueLabel) { $stopCommon.ContinueLabel = $ContinueLabel }
+	}
 	process {
-		#TODO: Implement
-		throw "Not Implemented Yet"
+		$sourceDirectoryPath = $Path
+		if ($Path -like '*.psd1') { $sourceDirectoryPath = Split-Path -Path $Path }
+
+		$moduleName = Split-Path -Path $sourceDirectoryPath -Leaf
+		if ($moduleName -match '^\d+(\.\d+){1,3}$') {
+			$moduleName = Split-Path -Path (Split-Path -Path $sourceDirectoryPath) -Leaf
+		}
+
+		#region Validation
+		$manifestPath = Join-Path -Path $sourceDirectoryPath -ChildPath "$moduleName.psd1"
+		if (-not (Test-Path -Path $manifestPath)) {
+			Stop-PSFFunction -String 'Copy-Module.Error.ManifestNotFound' -StringValues $Path -Target $Path @stopCommon -Category ObjectNotFound
+			return
+		}
+
+		$tokens = $null
+		$errors = $null
+		$ast = [System.Management.Automation.Language.Parser]::ParseFile($manifestPath, [ref]$tokens, [ref]$errors)
+
+		if ($errors) {
+			Stop-PSFFunction -String 'Copy-Module.Error.ManifestSyntaxError' -StringValues $Path -Target $Path @stopCommon -Category ObjectNotFound
+			return
+		}
+		#endregion Validation
+
+		#region Deploy to Staging
+		try { $null = New-Item -Path $Destination -Name $moduleName -ItemType Directory -ErrorAction Stop }
+		catch {
+			Stop-PSFFunction -String 'Copy-Module.Error.StagingFolderFailed' -StringValues $Path -Target $Path @stopCommon -ErrorRecord $_
+			return
+		}
+
+		$destinationPath = Join-Path -Path $Destination -ChildPath $moduleName
+		try { Copy-Item -Path "$($sourceDirectoryPath.Trim('\/'))\*" Destination $destinationPath -Recurse -Force -ErrorAction Stop }
+		catch {
+			Stop-PSFFunction -String 'Copy-Module.Error.StagingFolderCopy' -StringValues $Path -Target $Path @stopCommon -ErrorRecord $_
+			return
+		}
+		#endregion Deploy to Staging
+
+		$hashtableAst = $ast.EndBlock.Statements[0].PipelineElements[0].Expression
+		[PSCustomObject]@{
+			Name            = $moduleNamee
+			Path            = $destinationPath
+			SourcePath      = $sourceDirectoryPath
+			Author          = @($hashtableAst.KeyValuePairs).Where{ $_.Item1.Value -eq 'Author' }.Item2.PipelineElements.Expression.Value
+			Version         = @($hashtableAst.KeyValuePairs).Where{ $_.Item1.Value -eq 'ModuleVersion' }.Item2.PipelineElements.Expression.Value
+			Description     = @($hashtableAst.KeyValuePairs).Where{ $_.Item1.Value -eq 'Description' }.Item2.PipelineElements.Expression.Value
+			RequiredModules = Read-ManifestDependency -Path $manifestPath
+		}
 	}
 }
---
+++ PSFramework.NuGet/internal/functions/Get/Publish/Read-ManifestDependency.ps1
@@ -0,0 +1,46 @@
+﻿function Read-ManifestDependency {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $true)]
+		[string]
+		$Path
+	)
+	process {
+		$tokens = $null
+		$errors = $null
+		$ast = [System.Management.Automation.Language.Parser]::ParseFile($Path, [ref]$tokens, [ref]$errors)
+
+		$requirements = foreach ($requirement in @($ast.EndBlock.Statements[0].PipelineElements[0].Expression.KeyValuePairs).Where{ $_.Item1.Value -eq 'RequiredModules' }.Item2.PipelineElements.Expression.SubExpression.Statements) {
+			$actualRequirement = $requirement.PipelineElements[0].Expression
+			switch ($actualRequirement.GetType().Name) {
+				'HashtableAst' {
+					[PSCustomObject]@{
+						Name    = $actualRequirement.KeyValuePairs.Where{ $_.Item1.Value -eq 'ModuleName' }.Item2.PipelineElements.Expression.Value
+						Version = $actualRequirement.KeyValuePairs.Where{ $_.Item1.Value -match 'Version$' }.Item2.PipelineElements.Expression.Value -as [version]
+						Exact   = $actualRequirement.KeyValuePairs.Item1.Value -contains 'RequiredVersion'
+					}
+				}
+				'StringConstantExpressionAst' {
+					[PSCustomObject]@{
+						Name    = $actualRequirement.Value
+						Version = '0.0.0' -as [version]
+						Exact   = $false
+					}
+				}
+				default {
+					throw "Unexpected Module Dependency AST in $Path : $($actualRequirement.GetType().Name)"
+				}
+			}
+		}
+		foreach ($requirement in $requirements) {
+			if ($requirement.Exact -and -not $requirement.Version) {
+				Write-PSFMessage -Level Warning -String 'Read-ManifestDependency.Warning.VersionError' -StringValues $Path, $requirement.Name
+			}
+			if (-not $requirement.Version) {
+				$requirement.Version = '0.0.0' -as [version]
+			}
+
+			$requirement
+		}
+	}
+}
