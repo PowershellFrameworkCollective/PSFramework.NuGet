88a7372c7665d1a01c1779020501cd6dc8e8c900
FriedrichWeinmann <FriedrichWeinmann@users.noreply.github.com>
2025-01-03 03:26:21 +0100

    More resilient privatedata processing

--- PSFramework.NuGet/functions/Get/Update-PSFModuleManifest.ps1
+++ PSFramework.NuGet/functions/Get/Update-PSFModuleManifest.ps1
@@ -150,7 +150,7 @@
 						Stop-PSFFunction -String 'Update-PSFModuleManifest.Error.InvalidModuleReference' -StringValues $item -Target $item -EnableException $EnableException -Cmdlet $Cmdlet -Category InvalidArgument -Continue
 					}
 
-					$data = @{ ModuleName = $item.ModuleName }
+					$data = [ordered]@{ ModuleName = $item.ModuleName }
 					if ($item.RequiredVersion) { $data.RequiredVersion = '{0}' -f $item.RequiredVersion }
 					elseif ($item.ModuleVersion) { $data.ModuleVersion = '{0}' -f $item.ModuleVersion }
 
@@ -219,15 +219,98 @@
 			}
 			#endregion Case: Key Does not exist
 
-			$newText = $Ast.Extent.Text.SubString(0,$start) + ($format -f $stringValue) + $Ast.Extent.SubString($end)
+			$newText = $Ast.Extent.Text.SubString(0, $start) + ($format -f $stringValue) + $Ast.Extent.Text.SubString($end)
 			[System.Management.Automation.Language.Parser]::ParseInput($newText, [ref]$null, [ref]$null)
+		}
+		
+		function Update-PrivateDataProperty {
+			[OutputType([System.Management.Automation.Language.Ast])]
+			[CmdletBinding()]
+			param (
+				[Parameter(Mandatory = $true)]
+				[System.Management.Automation.Language.Ast]
+				$Ast,
+
+				[string[]]
+				$Tags,
+				
+				[string]
+				$LicenseUri,
+				
+				[string]
+				$IconUri,
+				
+				[string]
+				$ProjectUri,
+
+				[string]
+				$ReleaseNotes,
+
+				[string]
+				$Prerelease,
+
+				[object[]]
+				$ExternalModuleDependencies,
+
+				[bool]
+				$EnableException,
+				$Cmdlet
+			)
+
+			$mainHash = $Ast.FindAll({
+					$args[0] -is [System.Management.Automation.Language.HashtableAst] -and
+					$args[0].KeyValuePairs.Item1.Value -contains 'RootModule' -and
+					$args[0].KeyValuePairs.Item1.Value -Contains 'ModuleVersion'
+				}, $true)
+
+			$privateData = [ordered]@{
+				PSData = [ordered]@{ }
+			}
+			$replacements = @{ }
+	
+			$privateDataAst = $mainHash.KeyValuePairs | Where-Object { $_.Item1.Value -eq 'PrivateData' } | ForEach-Object { $_.Item2.PipelineElements[0].Expression }
+
+			if ($privateDataAst) {
+				foreach ($pair in $privateDataAst.KeyValuePairs) {
+					if ($pair.Item1.Value -ne 'PSData') {
+						$id = "%PSF_$(Get-Random)%"
+						$privateData[$pair.Item1.Value] = $id
+						$replacements[$id] = $pair.Item2.Extent.Text
+						continue
+					}
+
+					foreach ($subPair in $pair.Item2.PipelineElements[0].Expression.KeyValuePairs) {
+						$id = "%PSF_$(Get-Random)%"
+						$privateData.PSData[$subPair.Item1.Value] = $id
+						$replacements[$id] = $subPair.Item2.Extent.Text
+					}
+				}
+			}
+
+			if ($Tags) { $privateData.PSData['Tags'] = $Tags }
+			if ($LicenseUri) { $privateData.PSData['LicenseUri'] = $LicenseUri }
+			if ($IconUri) { $privateData.PSData['IconUri'] = $IconUri }
+			if ($ProjectUri) { $privateData.PSData['ProjectUri'] = $ProjectUri }
+			if ($ReleaseNotes) { $privateData.PSData['ReleaseNotes'] = $ReleaseNotes }
+			if ($Prerelease) { $privateData.PSData['Prerelease'] = $Prerelease }
+			if ($ExternalModuleDependencies) { $privateData.PSData['ExternalModuleDependencies'] = ConvertTo-ModuleRequirement -InputObject $ExternalModuleDependencies -Cmdlet $Cmdlet -EnableException $killIt }
+
+			$privateDataString = $privateData | ConvertTo-Psd1 -Depth 5
+			foreach ($pair in $replacements.GetEnumerator()) {
+				$privateDataString = $privateDataString -replace "'$($pair.Key)'", $pair.Value
+			}
+
+			if (-not $privateDataAst) {
+				$newManifest = $ast.Extent.Text.Insert(($mainHash.Extent.EndOffset - 1), "PrivateData = $privateDataString`n")
+			}
+			else {
+				$newManifest = $ast.Extent.Text.SubString(0, $privateDataAst.Extent.StartOffset) + $privateDataString + $ast.Extent.Text.SubString($privateDataAst.Extent.EndOffset)
+			}
+			[System.Management.Automation.Language.Parser]::ParseInput($newManifest, [ref]$null, [ref]$null)
 		}
 		#endregion Utility Functions
 	}
 	process {
-		# If Nothing to do, do nothing
-		if (-not ($Tags -or $LicenseUri -or $IconUri -or $ProjectUri -or $ReleaseNotes -or $Prerelease)) { return }
-
 		$killIt = $ErrorActionPreference -eq 'Stop'
 
 		$ast = [System.Management.Automation.Language.Parser]::ParseFile($Path, [ref]$null, [ref]$null)
@@ -244,17 +327,17 @@
 		}
 
 		#region Main Properties
-		$stringProperties = 'Guid','Author','CompanyName','CopyRight','RootModule','ModuleVersion','Description','ProcessorArchitecture','PowerShellVersion','ClrVersion','DotNetFrameworkVersion','PowerShellHostName','PowerShellHostVersion','HelpInfoUri','DefaultCommandPrefix'
+		$stringProperties = 'Guid', 'Author', 'CompanyName', 'CopyRight', 'RootModule', 'ModuleVersion', 'Description', 'ProcessorArchitecture', 'PowerShellVersion', 'ClrVersion', 'DotNetFrameworkVersion', 'PowerShellHostName', 'PowerShellHostVersion', 'HelpInfoUri', 'DefaultCommandPrefix'
 		foreach ($property in $stringProperties) {
 			if ($PSBoundParameters.Keys -notcontains $property) { continue }
 			$ast = Update-ManifestProperty -Ast $ast -Property $property -Value $PSBoundParameters.$property -Type String
 		}
-		$stringArrayProperties = 'CompatiblePSEditions','TypesToProcess','FormatsToProcess','ScriptsToProcess','RequiredAssemblies','FileList','FunctionsToExport','AliasesToExport','VariablesToExport','CmdletsToExport','DscResourcesToExport'
+		$stringArrayProperties = 'CompatiblePSEditions', 'TypesToProcess', 'FormatsToProcess', 'ScriptsToProcess', 'RequiredAssemblies', 'FileList', 'FunctionsToExport', 'AliasesToExport', 'VariablesToExport', 'CmdletsToExport', 'DscResourcesToExport'
 		foreach ($property in $stringArrayProperties) {
 			if ($PSBoundParameters.Keys -notcontains $property) { continue }
 			$ast = Update-ManifestProperty -Ast $ast -Property $property -Value $PSBoundParameters.$property -Type StringArray
 		}
-		$moduleProperties = 'RequiredModules','ModuleList','NestedModules'
+		$moduleProperties = 'RequiredModules', 'ModuleList', 'NestedModules'
 		foreach ($property in $moduleProperties) {
 			if ($PSBoundParameters.Keys -notcontains $property) { continue }
 			$ast = Update-ManifestProperty -Ast $ast -Property $property -Value ($PSBoundParameters.$property | ConvertTo-ModuleRequirement -EnableException $killIt -Cmdlet $Cmdlet) -Type StringArray
@@ -262,58 +345,15 @@
 		#endregion Main Properties
 		
 		#region PrivateData Content
-		$mainHash = $ast.FindAll({
-			$args[0] -is [System.Management.Automation.Language.HashtableAst] -and
-			$args[0].KeyValuePairs.Item1.Value -contains 'RootModule' -and
-			$args[0].KeyValuePairs.Item1.Value -Contains 'ModuleVersion'
-		}, $true)
-
-		$privateData = [ordered]@{
-			PSData = [ordered]@{ }
-		}
-		$replacements = @{ }
-		
-		$privateDataAst = $mainHash.KeyValuePairs | Where-Object { $_.Item1.Value -eq 'PrivateData' } | ForEach-Object { $_.Item2.PipelineElements[0].Expression }
-
-		if ($privateDataAst) {
-			foreach ($pair in $privateDataAst.KeyValuePairs) {
-				if ($pair.Item1.Value -ne 'PSData') {
-					$id = "%PSF_$(Get-Random)%"
-					$privateData[$pair.Item1.Value] = $id
-					$replacements[$id] = $pair.Item2.Extent.Text
-					continue
-				}
-
-				foreach ($subPair in $pair.Item2.PipelineElements[0].Expression.KeyValuePairs) {
-					$id = "%PSF_$(Get-Random)%"
-					$privateData.PSData[$subPair.Item1.Value] = $id
-					$replacements[$id] = $subPair.Item2.Extent.Text
-				}
-			}
-		}
-
-		if ($Tags) { $privateData.PSData['Tags'] = $Tags }
-		if ($LicenseUri) { $privateData.PSData['LicenseUri'] = $LicenseUri }
-		if ($IconUri) { $privateData.PSData['IconUri'] = $IconUri }
-		if ($ProjectUri) { $privateData.PSData['ProjectUri'] = $ProjectUri }
-		if ($ReleaseNotes) { $privateData.PSData['ReleaseNotes'] = $ReleaseNotes }
-		if ($Prerelease) { $privateData.PSData['Prerelease'] = $Prerelease }
-		if ($ExternalModuleDependencies) { $privateData.PSData['ExternalModuleDependencies'] = ConvertTo-ModuleRequirement -InputObject $ExternalModuleDependencies -Cmdlet $Cmdlet -EnableException $killIt }
-
-		$privateDataString = $privateData | ConvertTo-Psd1 -Depth 5
-		foreach ($pair in $replacements.GetEnumerator()) {
-			$privateDataString = $privateDataString -replace "'$($pair.Key)'", $pair.Value
-		}
-
-		if (-not $privateDataAst) {
-			$newManifest = $ast.Extent.Text.Insert(($mainHash.Extent.EndOffset - 1), "PrivateData = $privateDataString`n")
-		}
-		else {
-			$newManifest = $ast.Extent.Text.SubString(0, $privateDataAst.Extent.StartOffset) + $privateDataString + $ast.Extent.Text.SubString($privateDataAst.Extent.EndOffset)
+		if ($Tags -or $LicenseUri -or $IconUri -or $ProjectUri -or $ReleaseNotes -or $Prerelease -or $ExternalModuleDependencies) {
+			$updateParam = $PSBoundParameters | ConvertTo-PSFHashtable -ReferenceCommand Update-PrivateDataProperty
+			$updateParam.Cmdlet = $Cmdlet
+			$updateParam.EnableException = $killIt
+			$ast = Update-PrivateDataProperty -Ast $ast @updateParam
 		}
 		#endregion PrivateData Content
 
-		if ($PassThru) { $newManifest }
-		else { $newManifest | Set-Content -Path $Path }
+		if ($PassThru) { $ast.Extent.Text }
+		else { $ast.Extent.Text | Set-Content -Path $Path }
 	}
 }
